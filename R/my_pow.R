38499e86bcdcb17399765e44f8d936987043dd7b
harperzhu <harperzhu@yahoo.com>
2020-12-02 17:17:19 -0800

    Checking functions

--- R/my_pow.R
+++ R/my_pow.R
@@ -3,15 +3,17 @@
 #' This function power the input.
 #'
 #' @param x input to be converted to the \code{power}
+#' @param power Numeric input for the power that \code{x} will be raised to
+#'   defaults to \code{2}.
 #'
-#' @keywords temperature
+#' @keywords power
 #' @return Numeric representing \code{x} raised to the power of \code{power}
 #'
 #' @examples
-#' my_power(4)
-#' my_power(4, power =2)
+#' my_pow(4)
+#' my_pow(4, power =2)
 #'
 #' @export
 my_pow <- function(x, power =2){
-        return(x ^ power)
+        return(x^power)
 }
