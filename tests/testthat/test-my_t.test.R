442486ba8adc1ca8447924763df89416d7660714
harperzhu <harperzhu@yahoo.com>
2020-12-15 03:23:51 -0800

    add in coverage for t-test

--- tests/testthat/test-my_t.test.R
+++ tests/testthat/test-my_t.test.R
@@ -9,11 +9,15 @@ test_that("my_t.test produce numeric degree of freedom", {
 
 })
 test_that("my_t.test produce numeric degree of freedom", {
-        expect_is(my_t_test(x = my_gapminder$lifeExp,alternative = "less", mu = 60)$alternative, "character")
+        expect_is(my_t_test(x = my_gapminder$lifeExp,alternative = "greater", mu = 60)$alternative, "character")
 
 })
 test_that("my_t.test input throws error", {
         expect_error(my_t_test(x = my_gapminder$lifeExp,alternative = "less", mu = "string")$df)
+
+})
+test_that("my_t.test input throws message", {
+        expect_error(my_t_test(x = my_gapminder$lifeExp,alternative = "other", mu = 60)$df, "please try again")
 
 })
 # test_that("my_t.test produce numeric degree of freedom", {
